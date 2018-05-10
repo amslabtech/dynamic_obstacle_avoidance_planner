ad5d4c02b323a6f14473794dfd957f30505dd5e0
kazuki takahahsi <tkhsh.schoen@gmail.com>
2018-05-10 09:05:20 +0900

    Eigenを使ったオドメトリを実装.角度ずれる

--- four_wheel_drive_independent_steering/src/fwdis_gazebo.cpp
+++ four_wheel_drive_independent_steering/src/fwdis_gazebo.cpp
@@ -5,6 +5,7 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/JointState.h>
+#include <Eigen/Dense>
 #include "four_wheel_drive_independent_steering/FourWheelDriveIndependentSteering.h"
 
 std_msgs::Float64 frw;
@@ -35,8 +36,15 @@ bool joint_subscribed = false;
 
 const double WHEEL_RADIUS = 0.075;
 const double WHEEL_BASE = 0.50;
+const double L_2 = WHEEL_BASE / 2.0;
 const double TREAD = 0.50;
+const double B_2 = TREAD / 2.0;
 const double INTERVAL = 0.1;
+
+Eigen::MatrixXd forward_matrix;
+Eigen::MatrixXd inversed_matrix;
+Eigen::VectorXd wheel_velocity;//V1x, V1y, ...
+Eigen::Vector3d robot_velocity;//Vx, Vy, w
 
 void command_callback(const four_wheel_drive_independent_steering::FourWheelDriveIndependentSteeringConstPtr &msg)
 {
@@ -47,6 +55,28 @@ void joint_callback(const sensor_msgs::JointStateConstPtr &msg)
 {
   joint = *msg;
   joint_subscribed = true;
+}
+
+//https://robotics.naist.jp/edu/text/?Robotics%2FEigen#b3b26d13
+template <typename t_matrix>
+t_matrix PseudoInverse(const t_matrix& m, const double &tolerance=1.e-6)
+{
+  using namespace Eigen;
+  typedef JacobiSVD<t_matrix> TSVD;
+  unsigned int svd_opt(ComputeThinU | ComputeThinV);
+  if(m.RowsAtCompileTime!=Dynamic || m.ColsAtCompileTime!=Dynamic)
+  svd_opt= ComputeFullU | ComputeFullV;
+  TSVD svd(m, svd_opt);
+  const typename TSVD::SingularValuesType &sigma(svd.singularValues());
+  typename TSVD::SingularValuesType sigma_inv(sigma.size());
+  for(long i=0; i<sigma.size(); ++i)
+  {
+    if(sigma(i) > tolerance)
+      sigma_inv(i)= 1.0/sigma(i);
+    else
+      sigma_inv(i)= 0.0;
+  }
+  return svd.matrixV()*sigma_inv.asDiagonal()*svd.matrixU().transpose();
 }
 
 int main(int argc, char** argv)
@@ -73,6 +103,30 @@ int main(int argc, char** argv)
   odometry.child_frame_id = "base_link";
   odometry.pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
 
+  std::cout << "initialize matrix" << std::endl;
+
+  forward_matrix.resize(8, 3);
+  forward_matrix << 1.0, 0.0,  B_2,
+                    0.0, 1.0,  L_2,
+                    1.0, 0.0, -B_2,
+                    0.0, 1.0,  L_2,
+                    1.0, 0.0, -B_2,
+                    0.0, 1.0, -L_2,
+                    1.0, 0.0,  B_2,
+                    0.0, 1.0, -L_2;
+
+
+  std::cout << "forward:" << std::endl;
+  std::cout << forward_matrix << std::endl;
+
+  inversed_matrix.resize(3, 8);
+  inversed_matrix = PseudoInverse(forward_matrix);
+
+  wheel_velocity.resize(8, 1);
+
+  std::cout << "inversed:" << std::endl;
+  std::cout << inversed_matrix << std::endl;
+
   ros::Rate loop_rate(10);
 
   while(ros::ok()){
@@ -94,19 +148,29 @@ int main(int argc, char** argv)
     rrs_pub.publish(rrs);
     rls_pub.publish(rls);
 
+
     if(joint_subscribed){
       double vfr = joint.velocity[FRW] * WHEEL_RADIUS;
       double vfl = joint.velocity[FLW] * WHEEL_RADIUS;
       double vrl = joint.velocity[RLW] * WHEEL_RADIUS;
       double vrr = joint.velocity[RRW] * WHEEL_RADIUS;
-      double vfx = (vfr * cos(joint.position[FRS]) + vfl * cos(joint.position[FLS])) / 2.0;
-      double vfy = (vfr * sin(joint.position[FRS]) + vfl * sin(joint.position[FLS])) / 2.0;
-      double vrx = (vrr * cos(joint.position[RRS]) + vrl * cos(joint.position[RLS])) / 2.0;
-      double vry = (vrr * sin(joint.position[RRS]) + vrl * sin(joint.position[RLS])) / 2.0;
 
-      double omega = (vfy - vry) / WHEEL_BASE;
-      double vx = (vfx + vrx) / 2.0;
-      double vy = (vfy + vry) / 2.0;
+      wheel_velocity << vfr * cos(joint.position[FRS]),
+                        vfr * sin(joint.position[FRS]),
+                        vfl * cos(joint.position[FLS]),
+                        vfl * sin(joint.position[FLS]),
+                        vrl * cos(joint.position[RLS]),
+                        vrl * sin(joint.position[RLS]),
+                        vrr * cos(joint.position[RRS]),
+                        vrr * sin(joint.position[RRS]);
+
+      robot_velocity = inversed_matrix * wheel_velocity;
+
+      double omega = robot_velocity(2);
+      double vx = robot_velocity(0);
+      double vy = robot_velocity(1);
+      std::cout << "(vx, vy, w) = " << vx << ", " << vy << ", " << omega << std::endl;
+
       double yaw = tf::getYaw(odometry.pose.pose.orientation);
       odometry.pose.pose.position.x += (vx * cos(yaw) - vy * sin(yaw)) * INTERVAL;
       odometry.pose.pose.position.y += (vx * sin(yaw) + vy * cos(yaw)) * INTERVAL;
