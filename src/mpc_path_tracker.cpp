5119774b676e2f475faa7a87e1d2f4e50f302a27
Kazui Takahashi <tkhsh.schoen@gmail.com>
2018-07-24 14:43:09 +0900

    コスト以外実装

--- src/mpc_path_tracker.cpp
+++ src/mpc_path_tracker.cpp
@@ -1,5 +1,9 @@
+//ros
 #include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
 
+//ipopt
 #include <Eigen/Core>
 #include <cppad/cppad.hpp>
 #include <cppad/ipopt/solve.hpp>
@@ -33,8 +37,8 @@ const double DT = 0.1;
 //simulation parameters
 size_t x_start = 0;
 size_t y_start = x_start + T;
-size_t theta_start = y_start + T;
-size_t vx_start = theta_start + T;
+size_t yaw_start = y_start + T;
+size_t vx_start = yaw_start + T;
 size_t vy_start = vx_start + T;
 size_t omega_start = vy_start + T - 1;
 
@@ -43,6 +47,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "mpc_path_tracker");
   ros::NodeHandle nh;
   ros::NodeHandle local_nh("~");
+
+  MPC mpc;
 
   ros::Rate loop_rate(10);
 
@@ -58,7 +64,103 @@ MPC::MPC(){}
 
 std::vector<double> MPC::solve(Eigen::VectorXd state, Eigen::VectorXd coeffs)
 {
+  /*
+   * state:現在の値
+   * coeffs:係数行列
+   */
+  bool ok = true;
+  size_t i;
+  typedef CPPAD_TESTVECTOR(double) Dvector;
 
+  double x = state[0];
+  double y = state[1];
+  double yaw = state[2];
+
+  //3(x, y, yaw), 3(vx, vy, omega)
+  size_t n_variables = 3 * T + 3 * (T - 1);
+
+  size_t n_constraints = 3 * T;
+
+  Dvector vars(n_variables);
+  for(int i=0;i<n_variables;i++){
+    vars[i] = 0.0;
+  }
+
+  vars[x_start] = x;
+  vars[y_start] = y;
+  vars[yaw_start] = yaw;
+
+  Dvector vars_lower_bound(n_variables);
+  Dvector vars_upper_bound(n_variables);
+
+  for(int i=0;i<vx_start;i++){
+    //x, y, yaw
+    vars_lower_bound[i] = -1.0e19;
+    vars_upper_bound[i] = 1.0e19;
+  }
+  for(int i=vx_start;i<vy_start;i++){
+    vars_lower_bound[i] = 3.0;
+    vars_upper_bound[i] = -3.0;
+  }
+  for(int i=vy_start;i<omega_start;i++){
+    vars_lower_bound[i] = 3.0;
+    vars_upper_bound[i] = -3.0;
+  }
+  for(int i=omega_start;i<n_variables;i++){
+    vars_lower_bound[i] = 5.0;
+    vars_upper_bound[i] = -5.0;
+  }
+
+  //等式制約
+  Dvector constraints_lower_bound(n_constraints);
+  Dvector constraints_upper_bound(n_constraints);
+
+  for(int i=0;i<n_constraints;i++){
+    constraints_lower_bound[i] = 0.0;
+    constraints_upper_bound[i] = 0.0;
+  }
+
+  //t=0の設定
+  constraints_lower_bound[x_start] = x;
+  constraints_lower_bound[y_start] = y;
+  constraints_lower_bound[yaw_start] = yaw;
+
+  constraints_upper_bound[x_start] = x;
+  constraints_upper_bound[y_start] = y;
+  constraints_upper_bound[yaw_start] = yaw;
+
+  FG_eval fg_eval(coeffs);
+
+  std::string options;
+  options += "Integer print_level  0\n";
+
+  options += "Sparse  true        forward\n";
+  options += "Sparse  true        reverse\n";
+
+  options += "Numeric max_cpu_time          0.5\n";
+
+  CppAD::ipopt::solve_result<Dvector> solution;
+
+  CppAD::ipopt::solve<Dvector, FG_eval>(
+      options, vars, vars_lower_bound, vars_upper_bound, constraints_lower_bound,
+      constraints_upper_bound, fg_eval, solution);
+
+  ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+
+  auto cost = solution.obj_value;
+  std::cout << "Cost " << cost << std::endl;
+
+  std::vector<double> result;
+  result.push_back(solution.x[vx_start]);
+  result.push_back(solution.x[vy_start]);
+  result.push_back(solution.x[omega_start]);
+  //予測軌道
+  for(int i = 0; i < T-1; i++){
+    result.push_back(solution.x[x_start+i+1]);
+    result.push_back(solution.x[y_start+i+1]);
+    result.push_back(solution.x[yaw_start+i+1]);
+  }
+  return result;
 }
 
 FG_eval::FG_eval(Eigen::VectorXd coeffs)
@@ -70,16 +172,37 @@ void FG_eval::operator()(ADvector& fg, const ADvector& vars)
 {
   //cost
   fg[0] = 0;
-  for(int i=0;i<T-1;i++){
 
+  for(int i=0;i<T-1;i++){
+    //fg[0] += vars[i];
   }
+
   //constraint
   //初期状態
   fg[1 + x_start] = vars[x_start];
   fg[1 + y_start] = vars[y_start];
-  fg[1 + theta_start] = vars[theta_start];
+  fg[1 + yaw_start] = vars[yaw_start];
   fg[1 + vx_start] = vars[vx_start];
   fg[1 + vy_start] = vars[vy_start];
   fg[1 + omega_start] = vars[omega_start];
 
+  for(int i=0;i<T-1;i++){
+    //t+1
+    AD<double> x1 = vars[x_start + i + 1];
+    AD<double> y1 = vars[y_start + i + 1];
+    AD<double> yaw1 = vars[yaw_start + i + 1];
+    //t
+    AD<double> x0 = vars[x_start + i];
+    AD<double> y0 = vars[y_start + i];
+    AD<double> yaw0 = vars[yaw_start + i];
+    //入力ホライゾンはt+1を考慮しない
+    AD<double> vx0 = vars[vx_start + i];
+    AD<double> vy0 = vars[vy_start + i];
+    AD<double> omega0 = vars[omega_start + i];
+
+    //制約
+    fg[2 + x_start + i] = x1 - (x0 + vx0 * CppAD::cos(yaw0) * DT - vy0 * CppAD::sin(yaw0) * DT);
+    fg[2 + y_start + i] = y1 - (y0 + vx0 * CppAD::sin(yaw0) * DT + vy0 * CppAD::cos(yaw0) * DT);
+    fg[2 + yaw_start + i] = CppAD::atan2(CppAD::sin(yaw0 + omega0 * DT), CppAD::cos(yaw0 + omega0 * DT));
+  }
 }
