fe33da6cee4f10af6d08212235d9265afd1b4158
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-01-17 14:37:18 +0900

    update obs radius

--- fwdis/src/local_costmap.cpp
+++ fwdis/src/local_costmap.cpp
@@ -15,6 +15,8 @@ const double INTERVAL = 0.05;
 
 std::string ROBOT_FRAME;
 std::string WORLD_FRAME;
+double RADIUS;
+double RADIUS_GRID;
 
 nav_msgs::OccupancyGrid local_costmap;
 
@@ -56,6 +58,7 @@ int main(int argc, char** argv)
   ros::NodeHandle local_nh("~");
   local_nh.getParam("/dynamic_avoidance/ROBOT_FRAME", ROBOT_FRAME);
   local_nh.getParam("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME);
+  local_nh.getParam("/dynamic_avoidance/RADIUS", RADIUS);
 
   LocalCostmap local_costmap_creator;
   local_costmap_creator.process();
@@ -80,13 +83,36 @@ void LocalCostmap::process(void)
   while(ros::ok()){
     if(local_costmap_subscribed){
       if(cloud_updated){
+        nav_msgs::OccupancyGrid _map;
+        _map.data.resize(max_index);
         pcl_ros::transformPointCloud(ROBOT_FRAME, *cloud_ptr, *cloud_ptr, listener);
         int size = cloud_ptr->points.size();
         for(int i=0;i<size;i++){
           if(cloud_ptr->points[i].x < map_max_limit_x && cloud_ptr->points[i].x > map_min_limit_x && cloud_ptr->points[i].y < map_max_limit_y && cloud_ptr->points[i].y > map_min_limit_y){
             int index = get_index(cloud_ptr->points[i].x, cloud_ptr->points[i].y);
             if(index < max_index){
-              local_costmap.data[index] = 100;
+              _map.data[index] = 100;
+            }
+          }
+        }
+        for(int i=0;i<max_index;i++){
+          if(_map.data[i] == 100){
+            int _j = i % local_costmap.info.height;
+            int _k = i / local_costmap.info.height;
+            for(int j=0;j<local_costmap.info.height;j++){
+              for(int k=0;k<local_costmap.info.width;k++){
+                int dj = _j - j;
+                if(dj < 0){
+                  dj = -dj;
+                }
+                int dk = _k - k;
+                if(dk < 0){
+                  dk = -dk;
+                }
+                if(dj + dk <= RADIUS_GRID){
+                  local_costmap.data[k * local_costmap.info.height + j] = 100;
+                }
+              }
             }
           }
         }
@@ -115,6 +141,7 @@ void LocalCostmap::dynamic_callback(const nav_msgs::OccupancyGridConstPtr& msg)
   map_max_limit_y = local_costmap.info.resolution * local_costmap.info.width * 0.5;
   map_min_limit_y = -local_costmap.info.resolution * local_costmap.info.width * 0.5;
   max_index = local_costmap.info.width * local_costmap.info.height;
+  RADIUS_GRID = RADIUS / local_costmap.info.resolution;
   local_costmap_subscribed = true;
 }
 
