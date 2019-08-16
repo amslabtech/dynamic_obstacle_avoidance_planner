8fffcff236d926c7527e12640b493a1556351d51
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-08-16 19:38:57 +0900

    add publisher for PoseArray

--- src/diff_drive_sim.cpp
+++ src/diff_drive_sim.cpp
@@ -2,11 +2,13 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/tf.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseArray.h>
 #include <std_msgs/Int32.h>
 
 #include <random>
 
 std::vector<geometry_msgs::TransformStamped> obs_list;
+geometry_msgs::PoseArray obstacles;
 
 const double DT = 0.1;//[s]
 const double HZ = 100;
@@ -26,80 +28,93 @@ void update(int, double, double);
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "diff_drive_sim");
-  ros::NodeHandle nh;
-  ros::NodeHandle local_nh("~");
+    ros::init(argc, argv, "diff_drive_sim");
+    ros::NodeHandle nh;
+    ros::NodeHandle local_nh("~");
 
-  local_nh.getParam("NUM", NUM);
-  local_nh.getParam("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME);
-  local_nh.getParam("/dynamic_avoidance/OBSTACLES_FRAME", OBS_FRAME);
+    local_nh.getParam("NUM", NUM);
+    local_nh.getParam("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME);
+    local_nh.getParam("/dynamic_avoidance/OBSTACLES_FRAME", OBS_FRAME);
 
-  NUM = 1;
+    NUM = 1;
 
-  ros::Publisher obs_num_pub = nh.advertise<std_msgs::Int32>("/obs_num", 100);
+    ros::Publisher obs_num_pub = nh.advertise<std_msgs::Int32>("/obs_num", 1);
+    ros::Publisher obstacles_pub = nh.advertise<geometry_msgs::PoseArray>("/dynamic_obstacles", 1);
 
-  obs_list.resize(NUM);
+    obstacles.poses.resize(NUM);
+    obstacles.header.frame_id = WORLD_FRAME;
 
-  tf::TransformBroadcaster obs_broadcaster;
+    obs_list.resize(NUM);
 
-  // 初期位置設定
-  for(int i=0;i<NUM;i++){
-    obs_list[i].header.frame_id = WORLD_FRAME;
-    obs_list[i].child_frame_id = OBS_FRAME + std::to_string(i);
-    set_pose(i, 0, 0, 0);
-  }
-  set_pose(0, 20, -0.1, M_PI);
-  //set_pose(0, 2.5, 6.5, -M_PI/2.0);
-  //set_pose(1, 20, -0.4, M_PI);
-  //set_pose(0, -10, 7, 3*M_PI/2.0);
-  //set_pose(0, -8, 0, 0);
+    tf::TransformBroadcaster obs_broadcaster;
 
-  ros::Rate loop_rate(HZ);
+    // 初期位置設定
+    for(int i=0;i<NUM;i++){
+        obs_list[i].header.frame_id = WORLD_FRAME;
+        obs_list[i].child_frame_id = OBS_FRAME + std::to_string(i);
+        set_pose(i, 0, 0, 0);
+    }
+    set_pose(0, 20, -0.1, M_PI);
+    //set_pose(0, 2.5, 6.5, -M_PI/2.0);
+    //set_pose(1, 20, -0.4, M_PI);
+    //set_pose(0, -10, 7, 3*M_PI/2.0);
+    //set_pose(0, -8, 0, 0);
 
-  double sim_start_time = ros::Time::now().toSec();
+    ros::Rate loop_rate(HZ);
 
-  while(ros::ok()){
-    // 速度
-    //update(0, 1.0, 0);
-    update(0, 1.2, 0);
-    //update(1, 1.2, 0);
-    obs_broadcaster.sendTransform(obs_list);
+    double sim_start_time = ros::Time::now().toSec();
 
-    std_msgs::Int32 num;
-    num.data = NUM;
-    obs_num_pub.publish(num);
+    while(ros::ok()){
+        // 速度
+        //update(0, 1.0, 0);
+        update(0, 1.2, 0);
+        //update(1, 1.2, 0);
+        obs_broadcaster.sendTransform(obs_list);
+        obstacles_pub.publish(obstacles);
 
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  return 0;
+        std_msgs::Int32 num;
+        num.data = NUM;
+        obs_num_pub.publish(num);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
 }
 
 double get_yaw(geometry_msgs::Quaternion q)
 {
-  double r, p, y;
-  tf::Quaternion quat(q.x, q.y, q.z, q.w);
-  tf::Matrix3x3(quat).getRPY(r, p, y);
-  return y;
+    double r, p, y;
+    tf::Quaternion quat(q.x, q.y, q.z, q.w);
+    tf::Matrix3x3(quat).getRPY(r, p, y);
+    return y;
 }
 
 // map frame
 void set_pose(int index, double x, double y, double yaw)
 {
-  obs_list[index].header.stamp = ros::Time::now();
-  obs_list[index].transform.translation.x = x;
-  obs_list[index].transform.translation.y = y;
-  obs_list[index].transform.rotation = tf::createQuaternionMsgFromYaw(yaw);
+    obs_list[index].header.stamp = ros::Time::now();
+    obs_list[index].transform.translation.x = x;
+    obs_list[index].transform.translation.y = y;
+    obs_list[index].transform.rotation = tf::createQuaternionMsgFromYaw(yaw);
+
+    obstacles.poses[index].position.x = obs_list[index].transform.translation.x;
+    obstacles.poses[index].position.y = obs_list[index].transform.translation.y;
+    obstacles.poses[index].orientation = obs_list[index].transform.rotation;
 }
 
 // 各obs frame
 void update(int index, double v, double omega)
 {
-  v += dist(engine);
-  omega += dist(engine);
-  obs_list[index].header.stamp = ros::Time::now();
-  double yaw = tf::getYaw(obs_list[index].transform.rotation);
-  obs_list[index].transform.translation.x += v * cos(yaw) / HZ;
-  obs_list[index].transform.translation.y += v * sin(yaw) / HZ;
-  obs_list[index].transform.rotation = tf::createQuaternionMsgFromYaw(yaw + omega / HZ);
+    v += dist(engine);
+    omega += dist(engine);
+    obs_list[index].header.stamp = ros::Time::now();
+    double yaw = tf::getYaw(obs_list[index].transform.rotation);
+    obs_list[index].transform.translation.x += v * cos(yaw) / HZ;
+    obs_list[index].transform.translation.y += v * sin(yaw) / HZ;
+    obs_list[index].transform.rotation = tf::createQuaternionMsgFromYaw(yaw + omega / HZ);
+
+    obstacles.poses[index].position.x = obs_list[index].transform.translation.x;
+    obstacles.poses[index].position.y = obs_list[index].transform.translation.y;
+    obstacles.poses[index].orientation = obs_list[index].transform.rotation;
 }
