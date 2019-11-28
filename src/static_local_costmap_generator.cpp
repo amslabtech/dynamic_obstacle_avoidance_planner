89b080ca9fbcae2e151a4ec989c67d71c18f4e90
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-11-28 18:34:38 +0900

    update static_local_costmap_generator

--- src/static_local_costmap_generator.cpp
+++ src/static_local_costmap_generator.cpp
@@ -6,9 +6,10 @@ StaticLocalCostmapGenerator::StaticLocalCostmapGenerator(void)
     local_nh.getParam("/dynamic_avoidance/ROBOT_FRAME", ROBOT_FRAME);
     local_nh.getParam("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME);
     local_nh.getParam("/dynamic_avoidance/RADIUS", RADIUS);
-    local_nh.param("HZ", HZ, {20});
+    local_nh.param("RADIUS", RADIUS, {0.6});
+    local_nh.param("HZ", HZ, {10});
     local_nh.param("MAP_WIDTH", MAP_WIDTH, {20.0});
-    local_nh.param("RESOLUTION", RESOLUTION, {0.05});
+    local_nh.param("RESOLUTION", RESOLUTION, {0.1});
 
     map_pub = nh.advertise<nav_msgs::OccupancyGrid>("/local_costmap", 1);
     cloud_sub = nh.subscribe("/cluster/human/removed", 1, &StaticLocalCostmapGenerator::cloud_callback, this);
@@ -36,6 +37,7 @@ void StaticLocalCostmapGenerator::process(void)
     local_costmap.info.origin.position.y = -MAP_WIDTH / 2.0;
     local_costmap.info.origin.orientation = tf::createQuaternionMsgFromYaw(0.0);
     local_costmap.data.resize(local_costmap.info.width * local_costmap.info.height);
+    RADIUS_GRID = RADIUS / local_costmap.info.resolution;
     for(unsigned int i=0;i<local_costmap.info.height;i++){
         for(unsigned int j=0;j<local_costmap.info.width;j++){
             local_costmap.data[i*local_costmap.info.width+j] = 0;
@@ -45,10 +47,17 @@ void StaticLocalCostmapGenerator::process(void)
 
     while(ros::ok()){
         std::cout << "=== local_costmap ===" << std::endl;
+        double start = ros::Time::now().toSec();
         if(cloud_updated){
+            for(unsigned int i=0;i<local_costmap.info.height;i++){
+                for(unsigned int j=0;j<local_costmap.info.width;j++){
+                    local_costmap.data[i*local_costmap.info.width+j] = 0;
+                }
+            }
             pcl_ros::transformPointCloud(local_costmap.header.frame_id, *cloud_ptr, *cloud_ptr, listener);
             int cloud_size = cloud_ptr->points.size();
             std::cout << "cloud size: " << cloud_size << std::endl;
+            std::cout << "MAP_SIZE" << MAP_SIZE << std::endl;
             double map_max_limit_x = local_costmap.info.resolution * local_costmap.info.height * 0.5;
             double map_min_limit_x = -local_costmap.info.resolution * local_costmap.info.height * 0.5;
             double map_max_limit_y = local_costmap.info.resolution * local_costmap.info.width * 0.5;
@@ -61,24 +70,28 @@ void StaticLocalCostmapGenerator::process(void)
                     }
                 }
             }
+            nav_msgs::OccupancyGrid expanded_costmap = local_costmap;
             for(int i=0;i<MAP_SIZE;i++){
-                if(local_costmap.data[i]){
+                if(local_costmap.data[i] > 0){
                     int _j = i % local_costmap.info.height;
                     int _k = i / local_costmap.info.height;
                     for(unsigned int j=0;j<local_costmap.info.height;j++){
                         for(unsigned int k=0;k<local_costmap.info.width;k++){
                             int dj = _j - j;
                             int dk = _k - k;
-                            if(sqrt(dj*dj+dk*dk) <= RADIUS_GRID){
-                                local_costmap.data[k * local_costmap.info.height + j] = 100;
+                            if(abs(dj) <= RADIUS_GRID && abs(dk) <= RADIUS_GRID){
+                                if(sqrt(dj*dj+dk*dk) <= RADIUS_GRID){
+                                    expanded_costmap.data[k * local_costmap.info.height + j] = 100;
+                                }
                             }
                         }
                     }
                 }
             }
-            map_pub.publish(local_costmap);
+            map_pub.publish(expanded_costmap);
             cloud_updated = false;
         }
+        std::cout << "time: " << ros::Time::now().toSec() - start << "[s]" << std::endl;
         ros::spinOnce();
         loop_rate.sleep();
     }
@@ -89,6 +102,7 @@ void StaticLocalCostmapGenerator::cloud_callback(const sensor_msgs::PointCloud2C
     sensor_msgs::PointCloud2 _cloud;
     _cloud = *msg;
     pcl::fromROSMsg(_cloud, *cloud_ptr);
+    local_costmap.header.stamp = _cloud.header.stamp;
     cloud_updated = true;
 }
 
