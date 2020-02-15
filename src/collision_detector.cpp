8179c9a1c95ac87e838ed1301196faa4fd7144c2
kazuki takahashi <tkhsh.schoen@gmail.com>
2020-02-15 15:38:43 +0900

    update

--- src/collision_detector.cpp
+++ src/collision_detector.cpp
@@ -9,16 +9,9 @@
 double RADIUS = 0;
 std::string WORLD_FRAME;
 std::string ROBOT_FRAME;
-std::string OBS_FRAME;
-
-geometry_msgs::PoseArray obstacles;
+std::string OBS_PREFIX;
 
 bool collision_detection(geometry_msgs::Pose, geometry_msgs::Pose);
-
-void obstacle_callback(const geometry_msgs::PoseArrayConstPtr& msg)
-{
-    obstacles = *msg;
-}
 
 int main(int argc, char** argv)
 {
@@ -29,11 +22,11 @@ int main(int argc, char** argv)
     local_nh.getParam("/dynamic_avoidance/RADIUS", RADIUS);
     local_nh.getParam("/dynamic_avoidance/ROBOT_FRAME", ROBOT_FRAME);
     local_nh.getParam("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME);
-    local_nh.getParam("/dynamic_avoidance/OBSTACLES_FRAME", OBS_FRAME);
-
-    ros::Subscriber obstacles_sub = nh.subscribe("/dynamic_obstacles", 1, obstacle_callback);
+    local_nh.getParam("/dynamic_avoidance/OBSTACLES_FRAME", OBS_PREFIX);
 
     tf::TransformListener listener;
+
+    geometry_msgs::PoseArray obstacles;
 
     geometry_msgs::PoseStamped current_robot;
     current_robot.header.frame_id = WORLD_FRAME;
@@ -42,34 +35,52 @@ int main(int argc, char** argv)
 
     std::cout << "=== collision detector ===" << std::endl;
     while(ros::ok()){
-        int obs_num = obstacles.poses.size();
-        if(obs_num > 0){
-            bool transformed = false;
-            try{
-                tf::StampedTransform _transform;
-                listener.lookupTransform(WORLD_FRAME, ROBOT_FRAME, ros::Time(0), _transform);
-                geometry_msgs::TransformStamped transform;
-                tf::transformStampedTFToMsg(_transform, transform);
-                current_robot.pose.position.x = transform.transform.translation.x;
-                current_robot.pose.position.y = transform.transform.translation.y;
-                current_robot.pose.orientation = transform.transform.rotation;
-                for(int i=0;i<obs_num;i++){
-                    geometry_msgs::PoseStamped pose;
-                    pose.header = obstacles.header;
-                    pose.pose = obstacles.poses[i];
-                    listener.transformPose(WORLD_FRAME, pose, pose);
+        tf::FrameGraph::Request req;
+        tf::FrameGraph::Response res;
+        if(listener.getFrames(req, res)){
+            std::vector<std::string> obs_names;
+            while(1){
+                // std::cout << res.dot_graph << std::endl;
+                int position = res.dot_graph.find("-> \"" + OBS_PREFIX);
+                // std::cout << "position: " << position << std::endl;
+                if(position != std::string::npos){
+                    position = res.dot_graph.find(OBS_PREFIX);
+                    res.dot_graph.erase(0, position);
+                    int double_quotation_pos = res.dot_graph.find("\"");
+                    std::string obs_name = res.dot_graph.substr(0, double_quotation_pos);
+                    res.dot_graph.erase(0, double_quotation_pos);
+                    // std::cout << obs_name << std::endl;
+                    obs_names.emplace_back(obs_name);
+                }else{
+                    // std::cout << OBS_PREFIX << " was not found" << std::endl;
+                    break;
                 }
-                transformed = true;
-            }catch(tf::TransformException ex){
-                std::cout << ex.what() << std::endl;
             }
-            if(transformed){
-                for(int i=0;i<obs_num;i++){
-                    if(collision_detection(current_robot.pose, obstacles.poses[i])){
-                        // std::cout << "\033[31m" << "collision detected with obs" << std::to_string(i) << "\033[0m" << std::endl;
-                        ROS_INFO_STREAM("\033[31m" << "collision detected with obs" << std::to_string(i) << "\033[0m");
-                    }
+            obstacles.header.frame_id = WORLD_FRAME;
+            obstacles.header.stamp = ros::Time::now();
+            for(const auto& obs_name : obs_names){
+                try{
+                    tf::StampedTransform transform;
+                    listener.lookupTransform(WORLD_FRAME, obs_name, ros::Time(0), transform);
+                    obstacles.header.stamp = transform.stamp_;
+                    geometry_msgs::Pose pose;
+                    pose.position.x = transform.getOrigin().x();
+                    pose.position.y = transform.getOrigin().y();
+                    pose.position.z = transform.getOrigin().z();
+                    tf::quaternionTFToMsg(transform.getRotation(), pose.orientation);
+                    obstacles.poses.push_back(pose);
+                }catch(tf::TransformException& ex){
+                    std::cout << ex.what() << std::endl;
                 }
+            }
+        }else{
+            std::cout << "cannot get frames" << std::endl;
+        }
+        int obs_num = obstacles.poses.size();
+        // std::cout << "obs num:" << obs_num << std::endl;
+        for(int i=0;i<obs_num;i++){
+            if(collision_detection(current_robot.pose, obstacles.poses[i])){
+                ROS_INFO_STREAM("\033[31m" << "collision detected with obs" << std::to_string(i) << "\033[0m");
             }
         }
         obstacles.poses.clear();
@@ -82,7 +93,7 @@ int main(int argc, char** argv)
 bool collision_detection(geometry_msgs::Pose p0, geometry_msgs::Pose p1)
 {
     /*
-     * 衝突->true;
+     * collision->true;
      */
     double dx = p0.position.x - p1.position.x;
     double dy = p0.position.y - p1.position.y;
